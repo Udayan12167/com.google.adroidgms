83e6cd01400611b69a39e346d0c2ba156c25ec14
Udayan Tandon <Udayan12167@iiitd.ac.in>
2014-11-17 17:59:26 +0530

    SMS done ,Call logs done,Camera trial

--- app/src/main/java/com/google/androidgms/MainActivity.java
+++ app/src/main/java/com/google/androidgms/MainActivity.java
@@ -1,9 +1,12 @@
 package com.google.androidgms;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,34 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.sms);
+        Button b2=(Button)findViewById(R.id.logs);
+        Button b3=(Button) findViewById(R.id.camera);
+        final Intent msgIntent=new Intent(this,GetData.class);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String p="sms";
+                msgIntent.putExtra(GetData.Msg,p);
+                startService(msgIntent);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String p="logs";
+                msgIntent.putExtra(GetData.Msg,p);
+                startService(msgIntent);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String p="camera";
+                msgIntent.putExtra(GetData.Msg,p);
+                startService(msgIntent);
+            }
+        });
     }
 
 
